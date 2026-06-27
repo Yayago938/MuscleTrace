ed49d35129cbcfce67adf6638e0767192d3bde90
Yash Chauhan <yashuc007@gmail.com>
2026-06-27 21:51:27 +0530

    Frontend hosting integrations

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
-const port=5000;
+const port= process.env.PORT || 5000;;
 const app=express();
 const authroutes=require('./routes/authroutes')
 const authmiddleware=require('./middlewares/authmiddleware'); 
@@ -10,7 +10,12 @@ const exerciseRoutes=require('./routes/exerciseroutes');
 const progressRoutes=require('./routes/progressroutes');
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);;
 app.use('/api/auth',authroutes);
 app.use("/workouts", workoutRoutes)
 app.use("/exercises", exerciseRoutes)
